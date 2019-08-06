8667b76fbacc7d8638981383c50abb511d277061
Cory Gilliam <imaaxa@gmail.com>
2019-08-06 10:53:46 -0400

    Updated comments.

--- week-1/gilliam-exercise-1.3.js
+++ week-1/gilliam-exercise-1.3.js
@@ -14,7 +14,13 @@
 const header = require('../gilliam-header.js');
 console.log(header.display('Cory', 'Gilliam', 'Exercise 1.3'));
 
-//Using functions, create a cell phone object with the following properties / methods
+/**
+ * Create Cell Phone objects
+ * @param {string} manufacturer
+ * @param {string} model
+ * @param {string} color
+ * @param {float} price
+ */
 function CellPhone(manufacturer, model, color, price) {
   this.manufacturer = manufacturer;
   this.model = model;
@@ -22,19 +28,27 @@ function CellPhone(manufacturer, model, color, price) {
   this.price = price;
   }
 
+/**
+ * Get Price function for the CellPhone class
+ */
 CellPhone.prototype.getPrice = function () {
   return this.price;
 };
 
+/**
+ * Get Model function for the CellPhone class
+ */
 CellPhone.prototype.getModel = function () {
   return this.model;
 };
 
+/**
+ * Get Details function for the CellPhone class
+ */
 CellPhone.prototype.getDetails = function () {
   return 'Manufacturer: ' + this.manufacturer + '\nModel: ' + this.getModel() + '\nColor: ' + this.color + '\nPrice: $' + this.getPrice();
 };
 
 var myPhone = new CellPhone('Apple', 'iPhone X S Max', 'Rose Gold', 1099.00);
-var yourPhone = new CellPhone('Samsung', 'Galaxy S10', 'Black', 749.99);
+
 console.log(myPhone.getDetails());
-console.log(yourPhone.getDetails());

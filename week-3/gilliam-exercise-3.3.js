b3d1e9e1db917af0aaac266157a204e610a870b3
Cory Gilliam <imaaxa@gmail.com>
2019-08-22 10:58:18 -0400

    Added Exercise 3.3

---
+++ week-3/gilliam-exercise-3.3.js
@@ -0,0 +1,55 @@
+/*
+============================================
+; Title:  exercise-3.3.js
+; Author: Cory Gilliam
+; Date:   21 Aug 2019
+; Modified By:
+; Description: Use the singleton pattern to create new objects
+;===========================================
+*/
+
+// The header file, & output, to be included with all JS programs for this course/cohort.
+const header = require('../gilliam-header.js');
+console.log(header.display('Cory', 'Gilliam', 'Exercise 3.3'));
+/*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ Same database instance? true
+
+*/
+
+// start program
+var DatabaseSingleton = (function () {
+  // Holds the instance of the singleton
+  var instance;
+
+  // Creates a singleton
+  function createInstance() {
+    var postgresDatabase = new Object("Database instance initialized!");
+    return postgresDatabase;
+  }
+
+  // Create or return the singleton
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
+  };
+})();
+
+// Variables
+function databaseSingletonTest() {
+  var databaseInstance1 = DatabaseSingleton.getInstance();
+  var databaseInstance2 = DatabaseSingleton.getInstance();
+  console.log("Same database instance? " + (databaseInstance1 === databaseInstance2));
+}
+databaseSingletonTest();
+
+// end program
